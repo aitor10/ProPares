073f55ed3d14003e7aca06338a5b8fc7b46beffc
aitor10 <aittor98@gmail.com>
2020-04-17 18:03:41 +0200

    Segundo comente añadiendo Adios Mundo

--- ProPares1/src/ProPares01/ProPares1.java
+++ ProPares1/src/ProPares01/ProPares1.java
@@ -5,6 +5,8 @@ public class ProPares1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola Mundo");
+
+System.out.println(" Adios Mundo");
 	}
 
 }
